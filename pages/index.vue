56c85adef7aaa3c28063b866a3fc322bf6aa613c
Daniel <daniel.pe.15y15@gmail.com>
2023-12-01 11:40:40 -0300

    add todo to main page

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,37 @@
+<script setup>
+  const value = ref('')
+  const selected = ref(true)
+  let id = 0
+  const todoList = ref([
+    {id: id++, value: 'maçam'}, 
+    {id: id++, value: 'banana'}
+  ])
+  const newTodo = ref('')
+  function addTodo() {
+    todoList.value.push({id: id++, value: value.value})
+    value.value = ''
+  }
+
+  function removeTodo(todo) {
+    todoList.value = todoList.value.filter((t) => t !== todo)
+  }
+</script>
+
 <template>
-    <div>Index page</div>
-    <a class="nav-link active" aria-current="page" href="#">Active</a>
+  <div id="flexcon">
+    <div id="container">
+      <h2>Todo List</h2>
+      <div  id="inputcase">
+        <UInput v-model="value" id='inp'/>
+        <UButton @click="addTodo" id="btn">Adicionar ao todo</UButton>
+      </div>
+      <ul>
+        <li v-for="todo in todoList" :key="todo.id">
+          <UCheckbox  name="notifications" />
+          <p>{{ todo.value }}</p>
+          <UButton @click="removeTodo(todo)" color="black" variant="solid" size="2xs" icon="i-heroicons-x-mark-20-solid"/>
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
